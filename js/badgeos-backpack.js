a6ce09f3e4dae21904fb3f2154b21fcbbeabdd13
Martin Hawksey <m.hawksey@gmail.com>
2014-06-12 22:23:07 +0100

    Fixed Issuer API issue for IE

--- js/badgeos-backpack.js
+++ js/badgeos-backpack.js
@@ -1,15 +1,42 @@
 // JavaScript Document
+// Returns the version of Internet Explorer or a -1
+// (indicating the use of another browser).
+//
+// This code was taken from:
+//
+//   http://stackoverflow.com/a/17907562/1027723
+function getInternetExplorerVersion()
+{
+  var rv = -1;
+  if (navigator.appName == 'Microsoft Internet Explorer')
+  {
+    var ua = navigator.userAgent;
+    var re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+    if (re.exec(ua) != null)
+      rv = parseFloat( RegExp.$1 );
+  }
+  else if (navigator.appName == 'Netscape')
+  {
+    var ua = navigator.userAgent;
+    var re  = new RegExp("Trident/.*rv:([0-9]{1,}[\.0-9]{0,})");
+    if (re.exec(ua) != null)
+      rv = parseFloat( RegExp.$1 );
+  }
+  return rv;
+}
+
 jQuery(document).ready(function($){
 	badgeos_backpack_list();
 	// Our main achievement list AJAX call
 	$('.badgeos_backpack').live( 'click', function( event ) {
 		event.preventDefault();
 		$(this).attr("disabled", true);
-		OpenBadges.issue([$(this).attr('data-uid')], function(errors, successes) {
+		issueBadges([$(this).attr('data-uid')]);
+		/*OpenBadges.issue([$(this).attr('data-uid')], function(errors, successes) {
 			handle_backpack_response(errors, successes)
 			console.log(successes); // return full uid of successes in array
 			console.log(errors); // errors = [{assertion: "http://sites.hawksey.info/octel2/api/badge/assertion/?uid=100-1397727989-1",reason: "INACCESSIBLE"}];
-		});
+		});*/
 	});
 	
 	$('.badgeos_backpack_all').live( 'click', function( event ) {
@@ -18,11 +45,23 @@ jQuery(document).ready(function($){
 					  return this.value;
 					}).get() 
 		$(this).attr("disabled", true);
-		OpenBadges.issue(values, function(errors, successes) {
+		issueBadges(values)
+		/*OpenBadges.issue(values, function(errors, successes) {
 			handle_backpack_response(errors, successes)
 			console.log(successes);
-		});
+		});*/
 	});
+	
+	function issueBadges(assertions){
+		// Issuer API can't do modal in IE https://github.com/mozilla/openbadges/issues/1002
+		if (getInternetExplorerVersion() != -1){
+			OpenBadges.issue_no_modal(assertions);
+		} else {
+			OpenBadges.issue(assertions, function(errors, successes) {
+				handle_backpack_response(errors, successes)	
+			});
+		}
+	}
 	
 	function handle_backpack_response(errors, successes){
 		$.ajax({
