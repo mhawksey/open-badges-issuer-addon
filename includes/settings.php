ef1bd3fea6980b69c6996bf6c1122189bd100cb7
mhawksey <m.hawksey@gmail.com>
2014-06-17 15:22:22 +0100

    Fixed missing Open Badges missing from menu

--- includes/settings.php
+++ includes/settings.php
@@ -10,12 +10,8 @@ class BadgeOS_OpenBadgesIssuer_Settings {
 	 * Create BadgeOS Settings menus
 	 */
 	function plugin_menu() {
-
-		// Set minimum role setting for menus
-		$minimum_role = $badgeos_settings['minimum_role'];
-		
-		add_submenu_page( 'badgeos_badgeos', __( 'Open Badges Issuer Settings', 'badgeos_obi_issuer' ), __( 'Open Badges Issuer Settings', 'badgeos_obi_issuer' ), $minimum_role, 'open-badges-issuer', array(&$this, 'badgeos_obi_issuer_settings') );
-		add_submenu_page( 'badgeos_badgeos', __( 'Open Badges Issuer Log Entries', 'badgeos_obi_issuer' ), __( 'Open Badges Issuer Log Entries', 'badgeos_obi_issuer' ), $minimum_role, 'edit.php?post_type=open-badge-entry' );
+		add_submenu_page( 'badgeos_badgeos', __( 'Open Badges Issuer Settings', 'badgeos_obi_issuer' ), __( 'Open Badges Issuer Settings', 'badgeos_obi_issuer' ), badgeos_get_manager_capability(), 'open-badges-issuer', array(&$this, 'badgeos_obi_issuer_settings') );
+		add_submenu_page( 'badgeos_badgeos', __( 'Open Badges Issuer Log Entries', 'badgeos_obi_issuer' ), __( 'Open Badges Issuer Log Entries', 'badgeos_obi_issuer' ), badgeos_get_manager_capability(), 'edit.php?post_type=open-badge-entry' );
 	}
 	
 	function badgeos_obi_issuer_settings(){
